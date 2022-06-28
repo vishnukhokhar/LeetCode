f22588f65be297a96d2ab6b6570228228efe23bd
Vishnu Khokhar <72308651+vishnukhokhar@users.noreply.github.com>
2022-06-28 19:08:39 +0530

    Add files via upload

---
+++ ARRAY/max_subarray_sum.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+
+        int sum = 0;
+        int max_sum = INT_MIN;
+        for (auto it : nums)
+        {
+            sum += it;
+            max_sum = max(max_sum, sum);
+            if (sum < 0)
+            {
+                sum = 0;
+            }
+        }
+        return max_sum;
+    }
+};
