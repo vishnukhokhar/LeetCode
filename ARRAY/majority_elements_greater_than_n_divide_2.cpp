567948cb4f80d00948ea521ba56ef0a62ded5ac3
Vishnu Khokhar <72308651+vishnukhokhar@users.noreply.github.com>
2022-06-30 16:55:29 +0530

    Add files via upload

---
+++ ARRAY/majority_elements_greater_than_n_divide_2.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int majorityElement(vector<int> &nums)
+    {
+        int count = 0;
+        int candidate = 0;
+
+        for (int num : nums)
+        {
+            if (count == 0)
+            {
+                candidate = num;
+            }
+            if (candidate == num)
+            {
+                count += 1;
+            }
+            else
+            {
+                count -= 1;
+            }
+        }
+        return candidate;
+    }
+};
