b718afa0b4a2a70e9516c9d03c853d51a9e23189
Vishnu Khokhar <72308651+vishnukhokhar@users.noreply.github.com>
2022-07-02 16:32:52 +0530

    Add files via upload

---
+++ Linked List/reverse.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        ListNode *newhead = NULL;
+        while (head != NULL)
+        {
+            ListNode *next = head->next;
+            head->next = newhead;
+            newhead = head;
+            head = next;
+        }
+        return newhead;
+    }
+};
