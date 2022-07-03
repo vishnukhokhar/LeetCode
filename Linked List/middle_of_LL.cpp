84991612a4e2b106b8226e0b49f1df1f7b964726
Vishnu Khokhar <72308651+vishnukhokhar@users.noreply.github.com>
2022-07-03 23:53:18 +0530

    Add files via upload

---
+++ Linked List/middle_of_LL.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    ListNode *middleNode(ListNode *head)
+    {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while (fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
