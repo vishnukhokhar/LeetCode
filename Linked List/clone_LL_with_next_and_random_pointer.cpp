8f975f756837a3ca17089be81e5dad80d3a1d453
Vishnu Khokhar <72308651+vishnukhokhar@users.noreply.github.com>
2022-07-07 12:45:56 +0530

    Add files via upload

---
+++ Linked List/clone_LL_with_next_and_random_pointer.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution
+{
+public:
+    Node *copyRandomList(Node *head)
+    {
+        Node *iter = head;
+        Node *front = head;
+
+        while (iter != NULL)
+        {
+            front = iter->next;
+
+            Node *copy = new Node(iter->val);
+            iter->next = copy;
+            copy->next = front;
+
+            iter = front;
+        }
+
+        iter = head;
+
+        while (iter != NULL)
+        {
+            if (iter->random != NULL)
+            {
+                iter->next->random = iter->random->next;
+            }
+            iter = iter->next->next;
+        }
+
+        iter = head;
+        Node *pseudo = new Node(0);
+        Node *copy = pseudo;
+
+        while (iter != NULL)
+        {
+            front = iter->next->next;
+
+            copy->next = iter->next;
+            iter->next = front;
+            copy = copy->next;
+            iter = front;
+        }
+
+        return pseudo->next;
+    }
+};
