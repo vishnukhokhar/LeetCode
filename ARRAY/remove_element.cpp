dde5f51daae8c3b8bd900aafb0aca1eb490a3054
Vishnu Khokhar <72308651+vishnukhokhar@users.noreply.github.com>
2022-07-08 23:50:18 +0530

    Add files via upload

---
+++ ARRAY/remove_element.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int removeElement(vector<int> &nums, int val)
+    {
+        vector<int>::iterator it;
+        it = nums.begin();
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == val)
+            {
+                nums.erase(it);
+                it--;
+                i--;
+            }
+            it++;
+        }
+        return nums.size();
+    }
+};
