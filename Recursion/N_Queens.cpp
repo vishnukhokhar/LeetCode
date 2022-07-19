b4985109a9dfc5dd5813b903e8741337f22470c5
Vishnu Khokhar <72308651+vishnukhokhar@users.noreply.github.com>
2022-07-19 12:56:09 +0530

    Add files via upload

---
+++ Recursion/N_Queens.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    void solve(int col, vector<string> &board, vector<vector<string>> &ans, vector<int> &leftrow, vector<int> &upperdiagnal, vector<int> &lowerdiagnal, int n)
+    {
+        if (col == n)
+        {
+            ans.push_back(board);
+            return;
+        }
+
+        for (int row = 0; row < n; row++)
+        {
+            if (leftrow[row] == 0 && upperdiagnal[row + col] == 0 && lowerdiagnal[n - 1 + col - row] == 0)
+            {
+                board[row][col] = 'Q';
+                leftrow[row] = 1;
+                upperdiagnal[row + col] = 1;
+                lowerdiagnal[n - 1 + col - row] = 1;
+                solve(col + 1, board, ans, leftrow, upperdiagnal, lowerdiagnal, n);
+                board[row][col] = '.';
+                leftrow[row] = 0;
+                upperdiagnal[row + col] = 0;
+                lowerdiagnal[n - 1 + col - row] = 0;
+            }
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n)
+    {
+        vector<vector<string>> ans;
+        vector<string> board(n);
+        string s(n, '.');
+        for (int i = 0; i < n; i++)
+        {
+            board[i] = s;
+        }
+        vector<int> leftrow(n, 0);
+        vector<int> upperdiagnal(2 * n - 1, 0);
+        vector<int> lowerdiagnal(2 * n - 1, 0);
+        solve(0, board, ans, leftrow, upperdiagnal, lowerdiagnal, n);
+        return ans;
+    }
+};
