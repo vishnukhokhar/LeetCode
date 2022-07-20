b5dc507c6a403c0c06b09f3f1bcf876ca16fe592
Vishnu Khokhar <72308651+vishnukhokhar@users.noreply.github.com>
2022-07-20 19:57:47 +0530

    Add files via upload

---
+++ Binary Search/search_in_rotated_sorted_array.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+
+        int low = 0;
+        int high = nums.size() - 1;
+
+        while (low <= high)
+        {
+            int mid = (low + high) >> 1;
+            if (target == nums[mid])
+            {
+                return mid;
+            }
+
+            if (nums[low] <= nums[mid])
+            {
+                if (target >= nums[low] && target <= nums[mid])
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+
+            else
+            {
+                if (target >= nums[mid] && target <= nums[high])
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
