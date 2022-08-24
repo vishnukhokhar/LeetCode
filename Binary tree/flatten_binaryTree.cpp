9bcb6974971adf2943022bbb09d91433d534e523
Vishnu Khokhar <72308651+vishnukhokhar@users.noreply.github.com>
2022-08-24 22:44:01 +0530

    Add files via upload

---
+++ Binary tree/flatten_binaryTree.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+    TreeNode *prev = NULL;
+
+public:
+    void flatten(TreeNode *root)
+    {
+        TreeNode *curr = root;
+        while (curr)
+        {
+            if (curr->left)
+            {
+                TreeNode *pre = curr->left;
+                while (pre->right)
+                {
+                    pre = pre->right;
+                }
+                pre->right = curr->right;
+                curr->right = curr->left;
+                curr->left = NULL;
+            }
+            curr = curr->right;
+        }
+    }
+};
