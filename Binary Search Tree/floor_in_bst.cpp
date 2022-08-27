9eafc009f1d7b45601a6448d430372e9f6a32020
Vishnu Khokhar <72308651+vishnukhokhar@users.noreply.github.com>
2022-08-27 22:23:49 +0530

    Add files via upload

---
+++ Binary Search Tree/floor_in_bst.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T val;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T val) {
+            this->val = val;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+
+int floorInBST(TreeNode<int> *root, int X)
+{
+    int floor = -1;
+    while (root)
+    {
+        if (X == root->val)
+        {
+            floor = root->val;
+            return floor;
+        }
+        if (X > root->val)
+        {
+            floor = root->val;
+            root = root->right;
+        }
+        else
+        {
+            root = root->left;
+        }
+    }
+    return floor;
+    // Write your code here.
+}
