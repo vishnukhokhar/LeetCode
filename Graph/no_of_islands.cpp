c54e3fc2282d0a4d1f82a47bf2047fd4e3803faa
Vishnu Khokhar <72308651+vishnukhokhar@users.noreply.github.com>
2022-08-31 22:42:59 +0530

    Add files via upload

--- Graph/no_of_islands.cpp
+++ Graph/no_of_islands.cpp
@@ -7,7 +7,7 @@ private:
     {
         vis[row][col] = 1;
         queue<pair<int, int>> q;
-        q.push({row, col});
+        q.push(make_pair(row, col));
         int n = grid.size();
         int m = grid[0].size();
 
@@ -21,13 +21,15 @@ private:
             {
                 for (int delcol = -1; delcol <= 1; delcol++)
                 {
+                    if ((delrow == -1 && delcol == -1) || (delrow == -1 && delcol == 1) || (delrow == 1 && delcol == -1) || (delrow == 1 && delcol == 1))
+                        continue;
                     int nrow = row + delrow;
                     int ncol = col + delcol;
 
                     if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == '1' && !vis[nrow][ncol])
                     {
                         vis[nrow][ncol] = 1;
-                        q.push({nrow, ncol});
+                        q.push(make_pair(nrow, ncol));
                     }
                 }
             }
@@ -47,10 +49,10 @@ public:
         {
             for (int col = 0; col < m; col++)
             {
-                if (!vis[row][col] && grid[row][col] == '1')
+                if (vis[row][col] != 1 && grid[row][col] == '1')
                 {
-                    cnt++;
                     bfs(row, col, vis, grid);
+                    cnt++;
                 }
             }
         }
