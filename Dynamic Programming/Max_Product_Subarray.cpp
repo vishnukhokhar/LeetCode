9a788cec7f3e9b2356367dc121d30584d9c04258
Vishnu Khokhar <72308651+vishnukhokhar@users.noreply.github.com>
2022-10-20 14:34:14 +0530

    Add files via upload

---
+++ Dynamic Programming/Max_Product_Subarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int prod1=nums[0], prod2=nums[0], result=nums[0];
+        for(int i=1; i<nums.size();i++)
+        {
+            int temp=max({nums[i],prod1*nums[i],prod2*nums[i]});
+            prod2=min({nums[i],prod1*nums[i],prod2*nums[i]});
+                prod1=temp;
+            
+            result=max(result,prod1);
+        }
+        return result;
+        
+    }
+};
